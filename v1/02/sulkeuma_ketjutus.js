4bb19139071561c9f544fe421aa10d7ed5f1bde2
Simo Silander <simo.silander@metropolia.fi>
2017-03-21 15:29:44 +0000

    muutokset -> ecma6

--- v1/02/sulkeuma_ketjutus.js
+++ v1/02/sulkeuma_ketjutus.js
@@ -1,15 +1,18 @@
+'use strict';
+
 function grandparent(g1, g2) {
-	var g3 = 3;
+	let g3 = 3;
 	return function parent(p1, p2) {
-		var p3 = 33;
+		let p3 = 33;
 		return function child(c1, c2) {
-			var c3 = 333;
+			let c3 = 333;
 			return  " grand: " + g1 + g2 + g3 + 
 			" parent: " + p1 + p2 + p3 + 
 			" child:  " + c1 + c2 + c3;
-		}
-	}
+		};
+	};
 }
-var parentFunc = grandparent(1,2);
-var childFunc = parentFunc(11,22);
-console.log(childFunc(111,222));
+const parentFunc = grandparent(1,2);
+const childFunc = parentFunc(11,22);
+const tulos = childFunc(111,222);
+console.log(tulos);
