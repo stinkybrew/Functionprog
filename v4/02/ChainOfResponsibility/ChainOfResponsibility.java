0eca9a746780f189efe435d69ad92b3aeae4e9d5
Simo Silander <simo.silander@metropolia.fi>
2017-11-15 08:55:37 +0000

    Chain of resp koodin päivitys: toteutetaan immutablea

--- v4/02/ChainOfResponsibility/ChainOfResponsibility.java
+++ v4/02/ChainOfResponsibility/ChainOfResponsibility.java
@@ -19,8 +19,11 @@ public class ChainOfResponsibility {
     
         UnaryOperator<Pyynto> johtajaKasittelee =
                 (Pyynto p) -> 
-                    {   if (!p.isKasitelty() && p.getKorotuspyynto()/p.getPalkka() <= 0.10)
-                                    p.setPalkka(p.getPalkka()+p.getKorotuspyynto());
+                    {   if (!p.isKasitelty() && p.getKorotuspyynto()/p.getPalkka() <= 0.10){
+                                    System.out.println("Johtaja hyväksyy");  
+                                    p = new Pyynto(p.getPalkka()+p.getKorotuspyynto(), p.getKorotuspyynto());
+                                    p.setKasitelty(true);
+                        }
                         return p;
                     };
         
@@ -28,24 +31,27 @@ public class ChainOfResponsibility {
                 (Pyynto p) -> 
                     {   if (!p.isKasitelty() && p.getKorotuspyynto()/p.getPalkka() <= 0.05){
                             System.out.println("Paallikko hyväksyy");  
+                            p = new Pyynto(p.getPalkka()+p.getKorotuspyynto(), p.getKorotuspyynto());
                             p.setKasitelty(true);
-                            p.setPalkka(p.getPalkka()+p.getKorotuspyynto());
                         }
                         return p;
                     };
+        
         UnaryOperator<Pyynto> esimiesKasittelee =
                 (Pyynto p) -> 
                     {   if (p.getKorotuspyynto()/p.getPalkka() <= 0.02){
                             System.out.println("Esimies hyväksyy");  
+                            p = new Pyynto(p.getPalkka()+p.getKorotuspyynto(), p.getKorotuspyynto());
                             p.setKasitelty(true);
-                            p.setPalkka(p.getPalkka()+p.getKorotuspyynto());
                         }
                         return p;
                     };
         
+
+        
         Function<Pyynto, Pyynto> ketju = esimiesKasittelee.andThen(paallikkoKasittelee).andThen(johtajaKasittelee); 
         
-        Pyynto p = new Pyynto(1000, 10);
+        Pyynto p = new Pyynto(1000, 100);
         
         p = ketju.apply(p);
         
