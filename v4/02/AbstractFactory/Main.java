f6b9194dcc21a914847f7e1cc56d5fda9b02bfbf
Simo Silander <simo.silander@metropolia.fi>
2017-11-20 07:43:52 +0000

    Testiaineistomuutoksia

--- v4/02/AbstractFactory/Main.java
+++ v4/02/AbstractFactory/Main.java
@@ -14,7 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
        
-        AbstractShapeFactory factory = new ShapeFactory();
+        AbstractShapeFactory factory = new ShapeFactory3D();
         
         Shape shape1 = factory.getShape("circle");
         Shape shape2 = factory.getShape("rectangle");
