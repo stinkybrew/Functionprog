e114ae9329e55a5e0af850cad369f1b77c5b8b35
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-19 09:04:05 +0200

    ...
    ...

--- v1/01/fact.js
+++ v1/01/fact.js
@@ -1,10 +1,15 @@
 function fact(n) {
 // triviaalitapaus
-  if (n === 0) {
+  if (n.length === 0||1) {
     return 1;
+  }
+  else if (n.charAt(0) !== n.charAt(-1)) {
+    return 0;
+
+
   }
  // perussilmukka
   return n * fact(n - 1);
 }
-var tulos = fact(4);
+var tulos = fact();
 console.log(tulos);
